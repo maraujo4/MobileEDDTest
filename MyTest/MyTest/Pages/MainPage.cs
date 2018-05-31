391e4ccde7a5c7b0cdc57dd1f096547fec3b95c8
maraujo4 <maraujo4@users.noreply.github.com>
2018-05-31 15:30:16 -0400

    Add files via upload

---
+++ MyTest/MyTest/Pages/MainPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyTest.Pages
+{
+    public class MainPage : ContentPage
+    {
+        public Entry m_entryFirstName = new Entry();
+        public Entry m_entryLasttName = new Entry();
+        public Button m_buttonConcatNames;
+        public Label m_lableMergeResult = new Label();
+        public MainPage()
+        {
+            Label labelFirstName = new Label
+            {
+                Text = "First Name"
+            };
+
+            Label labelLastName = new Label
+            {
+                Text = "Last Name"
+            };
+
+            m_buttonConcatNames = new Button
+            {
+                Text = "Click me!"
+            };
+            m_buttonConcatNames.Clicked += M_buttonConcatNames_Clicked;
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    labelFirstName,
+                    m_entryFirstName,
+                    labelLastName,
+                    m_entryLasttName,
+                    m_buttonConcatNames,
+                    m_lableMergeResult
+                }
+            };
+        }
+
+        private void M_buttonConcatNames_Clicked(object sender, EventArgs e)
+        {
+            try
+            {                
+                m_lableMergeResult.Text = $"{m_entryFirstName.Text} {m_entryLasttName.Text}";
+            }
+            catch (Exception ex)
+            {
+                m_lableMergeResult.Text = ex.Message;
+            }
+        }
+    }
+}
